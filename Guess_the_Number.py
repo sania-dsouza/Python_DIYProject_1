352fcb04694edeecf512a4161fee0186d0d91c1f
Sania <sania.d@myplanet.com>
2019-10-29 17:08:08 -0400

    Initial commit to the guess number game

---
+++ Guess_the_Number.py
@@ -0,0 +1,16 @@
+#This is the Guess the  number game which allows the user to guess a number between 0 and 20. 
+#When the user selects a number, the computer tells the user if the guess was "too high" or "too low"
+
+# generate a random number using the <>
+
+
+# get the number from the user
+
+
+# compare it with the number
+
+
+# give the user a response as to whether the guess is too high or too low
+
+
+# Repeat the guessing for three tries
