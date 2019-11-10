9a0f17d268e011344d8601a284f45773b88a29a5
Sania <sania.d@myplanet.com>
2019-11-09 23:32:33 -0500

    Final changes to the password generator using random.sample.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,10 +1,12 @@
 #This program will generate a password for you according to the rules you specify
 #the only pre-existing rule is that the password has to be atleast 6 characters long
 
+import random
+
 # Determine the length of the password
 while(1):
     try:
-        passL = input("Enter the length of the desired password in the format 'minL-maxL':")
+        passL = int(input("Enter the length of the desired password:"))
     except Exception as e:
         print("Retry please. Follow the format for the password length specification.")
     else:
@@ -13,19 +15,12 @@ while(1):
         pass
 
 # Determine if the password must include special characters, numbers and upper-case, lower-case characters (separately)
-passChar = input("Can the password have special characters? (y/n): ")
-passNum = input("Can the password have numbers? (y/n): ")
-passUpLoMix = input("Can the password have a mix of upper and lower characters? (y/n): ")
-if!(passUpLoMix): 
-    passLo = input("Lower-case characters? (y/n): ")
+s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+passwd =  "".join(random.sample(s,passL))
+print(passwd)
 
-passwordGen(passL, passChar, passNum, passUpLoMix, passLo)
 
-# General password rules this program follows:
-# The password that is generated has a minimum of 2 numbers, 2 upper-case letters, 
-# two special characters and the rest are lower-case characters
-def passwordGen(passL, passChar, passNum, passUpLoMix, passLo):    
-    
+
 
 
 
