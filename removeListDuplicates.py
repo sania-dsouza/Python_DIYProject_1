f6869880730c710bd454cad48043d5b90a6221de
Sania <sania.d@myplanet.com>
2019-11-19 16:13:50 -0500

    Remove duplicates from a list

---
+++ removeListDuplicates.py
@@ -0,0 +1,14 @@
+#Remove the duplicates of a list using two methods
+
+# Method 1: Convert list to set
+listA = [2,2, 3, 4,5, 4, 5, 6]
+setA = set(listA)
+listA = list(setA)
+print("Using sets:", listA)
+
+# Method 2: Using lists
+listB = []
+for i in listA:
+    if i not in listB:listB.append(i)
+    else: continue
+print("Using lists:", sorted(listB))
