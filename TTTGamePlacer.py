f04146b2209871e84a5cd15e0219b1e6cb174d1d
Sania <sania.d@myplanet.com>
2019-11-22 11:00:43 -0500

    Part 1 of changes to the TTT Board placer

--- TTTGamePlacer.py
+++ TTTGamePlacer.py
@@ -5,11 +5,20 @@
 # Player 2: o
 # Chances alternate between player 1 and 2
 
+
+# Changes to be made:
+#1 Make the input requesting end when the number empty blocks goes to 1
+#2 Auto-fill the last character
+#3 display the board in board format after each placement
+#4 connected with #1 and #2: repeat a character input and placement if the previous try was invalid
+
 # Function that places the x's and o's in position on the row
 def rowPlacerDef(gameRes, row, col, char):
     # This function checks if the row, col on the board is valid for placement
     if gameRes[row][col] != "-":
         print("{0} can't be placed here because the position is already taken.".format(char))
+        # This chance must not be counted and the user must be given another chance if the position 
+        # entered was not valid
     else:
         gameRes[row][col] = char
         
@@ -22,7 +31,7 @@ gameRes[0] = ['-', '-', '-']  # initialise the starting list which is going to b
 gameRes[1] = ['-', '-', '-']
 gameRes[2] = ['-', '-', '-']
 
-for i in range(4): # 9 is the total places to be filled, 4 chances per player to fill the board
+while (gameRes[0].count("-")+gameRes[1].count("-")+gameRes[2].count("-")) > 0:
 
     # for player 1
     char = 'x'
@@ -36,4 +45,7 @@ for i in range(4): # 9 is the total places to be filled, 4 chances per player to
     gameRes = rowPlacerDef(gameRes, row, col, char)
     print(gameRes)
 
-#print([i for i in gameRes])
+    #print(gameRes[0].count("-")+gameRes[1].count("-")+gameRes[2].count("-"))
+
+for i in gameRes:
+    print(i)
