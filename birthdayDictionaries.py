ec981b065d7cd835ee42fde4dbb1181cc6a7597f
Sania <sania.d@myplanet.com>
2019-11-26 15:31:57 -0500

    Changes to birthday dict part 1

--- birthdayDictionaries.py
+++ birthdayDictionaries.py
@@ -3,12 +3,10 @@
 nameDict = {} # main dict with the names and the birthdays.
 
 # get the user input to fill the dictionary  (10 birthdays for now)
-for i in range(2):
+for i in range(5):
     name = str(input("Name of the personality:"))
     bday = str(input("Birthday with separator '-':"))
     nameDict[name] = bday
-    #print(nameDict)
-    #print(nameDict[name])
 
 print("Know the birthday of a famous persoanlity:")
 person = input("Name of the personality:")
